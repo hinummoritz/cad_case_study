19902acbec3ed7bb81bae1c352de8d6080611bb1
Moritz Hinum <138589559+hinummoritz@users.noreply.github.com>
2024-08-07 12:58:33 +0200

    # impl exercise comments

--- freecad_script.py
+++ freecad_script.py
@@ -2,6 +2,12 @@ import FreeCAD as App
 import Part
 import Draft
 
+# Grundsätzliches Vorgehen:
+# Schreibe eine Funktion für jede Aufgabe, die die benötigten Parameter entgegennimmt und das Ergebnis zurückgibt.
+# printe das Ergebnis der Funktionen in der Konsole aus.
+
+# Aufgabe1 : Erstelle eine Funktion, welches ein 3D-Modell aus einer STEP-Datei lädt, initialisere hierfür ein neues Dokument und füge das 3D-Modell hinzu.
+# Verwende hierfür die App und Part-Module von FreeCAD.
 def load_step_file(step_file_path):
 
     #Initialize the document
@@ -20,6 +26,7 @@ def load_step_file(step_file_path):
     return doc
 
 
+# Aufgabe 2 : Erstelle eine Funktion, die die Ausdehnung des 3D-Modells in X-, Y- und Z-Richtung zurückgibt. Benutze hierfür dein Part Objekt aus Aufgabe 1.
 def get_extents(doc):
 
     boundingBox=doc.Object3D.Shape.BoundBox
@@ -27,7 +34,7 @@ def get_extents(doc):
 
     return extents
 
-    
+# Fleissaufgabe: Erstelle eine Funktion, die das 3D-Modell in eine 2D-Ansicht kollabiert. Benutze hierfür das Draft-Modul von FreeCAD.
 def collapse_to_2D(doc):
 
     shape_2d = Draft.make_shape2dview(doc.Object3D, App.Vector(0, 0, 1))
@@ -42,6 +49,6 @@ doc = load_step_file(step_file_path)
 extents = get_extents(doc)
 objs = collapse_to_2D(doc)
 
-print(extents, objs)
+print(extents, objs, doc.Object3D)
 
 
